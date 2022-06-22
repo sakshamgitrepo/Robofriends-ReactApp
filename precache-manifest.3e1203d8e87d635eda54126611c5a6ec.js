ea2bce43e8fd2bf995a465abdb87d92568800ca3
Saksham <sakshamraj61@gmail.com>
2022-06-22 21:16:14 +0530

    Updates

---
+++ precache-manifest.3e1203d8e87d635eda54126611c5a6ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86db878a484ae3751f88",
+    "url": "/RoboFriends/static/css/main.e1817dce.chunk.css"
+  },
+  {
+    "revision": "86db878a484ae3751f88",
+    "url": "/RoboFriends/static/js/main.86db878a.chunk.js"
+  },
+  {
+    "revision": "cfd503847a738def7ae4",
+    "url": "/RoboFriends/static/css/1.0f429b1e.chunk.css"
+  },
+  {
+    "revision": "cfd503847a738def7ae4",
+    "url": "/RoboFriends/static/js/1.cfd50384.chunk.js"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ad677b35203fcc571cce2ce9c166c9cf",
+    "url": "/RoboFriends/index.html"
+  }
+];
